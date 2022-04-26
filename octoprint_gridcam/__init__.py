a9da66a58b3958c906b250af67960b74e381ca6f
kanstancin <ceitmo@gmail.com>
2022-04-26 18:12:38 -0400

    fix

--- octoprint_gridcam/__init__.py
+++ octoprint_gridcam/__init__.py
@@ -112,7 +112,8 @@ class GridCamPlugin(octoprint.plugin.StartupPlugin,
 
     def __init__(self):
         self.cam = ''
-        self.stream = ""
+        self.stream = ''
+        self.bytes = b''
 
     def on_after_startup(self):
         self._logger.info("GridCam \n(more: %s)" % self._settings.get(["url"]))
@@ -224,13 +225,12 @@ class GridCamPlugin(octoprint.plugin.StartupPlugin,
         #ret, img = self.cam.read()
         ret = False
         img = None
-        bytes = b''
-        bytes += self.stream.read(1024)
-        a = bytes.find(b'\xff\xd8')  # frame starting
-        b = bytes.find(b'\xff\xd9')  # frame ending
+        self.bytes += self.stream.read(1024)
+        a = self.bytes.find(b'\xff\xd8')  # frame starting
+        b = self.bytes.find(b'\xff\xd9')  # frame ending
         if a != -1 and b != -1:
-            jpg = bytes[a:b + 2]
-            bytes = bytes[b + 2:]
+            jpg = self.bytes[a:b + 2]
+            bytes = self.bytes[b + 2:]
             img = cv.imdecode(np.fromstring(jpg, dtype=np.uint8), 1)
             ret = True
             # cv.imshow('image', img)
